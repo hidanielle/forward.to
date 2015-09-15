edc95b1f0cfeec183371cb0bb7717cc3954de4bf
Danielle.Graham <danielle.graham@mosaic.com>
2015-09-15 13:38:13 -0400

    initial commit

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,20 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', ['$scope', function($scope) {
+  $scope.orderProp = 'date';
+  $scope.occasions = [
+    {'name': '50th Wedding Anniversary',
+     'date': new Date('2015-09-19T04:00:00.000Z')},
+    {'name': 'House Warming',
+     'date': new Date('2015-09-26T04:00:00.000Z')},
+    {'name': 'Halloween Haunt',
+     'date': new Date('2015-10-02T04:00:00.000Z')}
+  ];
+  $scope.addOccasion = function() {
+    $scope.occasions.push({
+      'name': $scope.occasion.name,
+      'date': $scope.occasion.date 
+    });
+  }
 }]);
